87471b96456d6c73b98a55d17ca065ae46f87a1c
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-07-29 12:06:26 -0300

    infra: project basic structure created

---
+++ src/test/java/com/bcarsoftware/avantemove_api/AvantemoveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcarsoftware.avantemove_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvantemoveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
