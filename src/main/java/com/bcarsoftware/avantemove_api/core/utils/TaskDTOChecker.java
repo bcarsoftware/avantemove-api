24b77f6edb397e940780f5c9c8e97367670af8d0
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-07-30 12:46:25 -0300

    feat: task dto checker

---
+++ src/main/java/com/bcarsoftware/avantemove_api/core/utils/TaskDTOChecker.java
@@ -0,0 +1,18 @@
+package com.bcarsoftware.avantemove_api.core.utils;
+
+import com.bcarsoftware.avantemove_api.dtos.TaskDTO;
+import com.bcarsoftware.avantemove_api.exceptions.BodyException;
+
+import java.util.List;
+
+public class TaskDTOChecker {
+    public static void taskDTOChecker(TaskDTO taskDTO) {
+        var failed = List.of(
+            !taskDTO.comment().matches("^[a-zA-Z_0-9][a-zA-Z-_ ,.0-9]{1,510}[a-zA-Z.0-9]$"),
+            taskDTO.comment().matches(" {2,}")
+        );
+
+        if (failed.contains(true))
+            throw new BodyException("task comment characters not match");
+    }
+}
--- src/main/java/com/bcarsoftware/avantemove_api/core/utils/UserDTOChecker.java
+++ src/main/java/com/bcarsoftware/avantemove_api/core/utils/UserDTOChecker.java
@@ -39,7 +39,7 @@ public class UserDTOChecker {
         Map<String, String> regex = new HashMap<>();
 
         regex.put("email",
-                "^[a-z][a-z0-9._-]{1,254}[a-z0-9]@[a-z0-9]{1,16}(.[a-z]{2,3}){1,2}$"
+            "^[a-z][a-z0-9._-]{1,254}[a-z0-9]@[a-z0-9]{1,16}(.[a-z]{2,3}){1,2}$"
         );
 
         regex.put("username", "^[a-z][a-z-_0-9]{1,254}$");
@@ -51,7 +51,7 @@ public class UserDTOChecker {
         regex.put("lastName", "^[A-Z][a-zA-Z- .]{1,253}[a-zA-Z.]$");
 
         regex.put("description",
-                "^[a-zA-Z_0-9][a-zA-Z-_ ,.0-9]{1,510}[a-zA-Z.0-9]$"
+            "^[a-zA-Z_0-9][a-zA-Z-_ ,.0-9]{1,510}[a-zA-Z.0-9]$"
         );
 
         return regex;
