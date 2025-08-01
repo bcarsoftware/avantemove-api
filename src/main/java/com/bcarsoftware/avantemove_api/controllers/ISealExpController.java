a0423500e62fcf0e593b19c984ebb530cb95b7ff
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-01 12:16:04 -0300

    feat: seal exp controller interface created

---
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/ISealExpController.java
@@ -0,0 +1,16 @@
+package com.bcarsoftware.avantemove_api.controllers;
+
+import com.bcarsoftware.avantemove_api.dtos.SealExpDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface ISealExpController {
+    public abstract ResponseEntity<?> save(@RequestBody SealExpDTO sealExpDTO);
+    public abstract ResponseEntity<?> getAllSealExp();
+    public abstract ResponseEntity<?> update(
+        @PathVariable Long id,
+        @RequestBody SealExpDTO sealExpDTO
+    );
+    public abstract ResponseEntity<?> getSealExpByUserExperience(@PathVariable Long experience);
+}
