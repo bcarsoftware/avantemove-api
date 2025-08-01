3e524253e2f730b8cf017f19d108e5ff11b2f210
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-01 12:30:40 -0300

    feat: task controller interface created

---
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/ITaskController.java
@@ -0,0 +1,17 @@
+package com.bcarsoftware.avantemove_api.controllers;
+
+import com.bcarsoftware.avantemove_api.dtos.TaskDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface ITaskController {
+    public abstract ResponseEntity<?> save(@RequestBody TaskDTO taskDTO);
+    public abstract ResponseEntity<?> getTaskByHabit(@PathVariable Long habitId);
+    public abstract ResponseEntity<?> update(
+        @PathVariable Long id,
+        @RequestBody TaskDTO taskDTO
+    );
+    public abstract ResponseEntity<?> finish(@PathVariable Long id);
+    public abstract ResponseEntity<?> delete(@PathVariable Long id);
+}
