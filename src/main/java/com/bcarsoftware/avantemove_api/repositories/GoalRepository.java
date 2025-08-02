2c0233b710b58c405ca9d68f0aed1fdf0a928884
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-02 18:23:14 -0300

    feat: goal repository implemented

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/GoalRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/GoalRepository.java
@@ -1,4 +1,10 @@
 package com.bcarsoftware.avantemove_api.repositories;
 
-public interface GoalRepository {
+import com.bcarsoftware.avantemove_api.models.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+    public abstract List<Goal> findGoalByUserId(Long userId);
 }
