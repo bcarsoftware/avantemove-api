60ff12a7b86cb2391854197d5c4355f09914b9e8
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-04 13:11:29 -0300

    feat: habit repository implemented

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/HabitRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/HabitRepository.java
@@ -3,5 +3,9 @@ package com.bcarsoftware.avantemove_api.repositories;
 import com.bcarsoftware.avantemove_api.models.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HabitRepository extends JpaRepository<Habit, Long> {
+    public abstract List<Habit> findHabitByUserId(Long id);
+    public abstract List<Habit> findHabitByGoalId(Long id);
 }
