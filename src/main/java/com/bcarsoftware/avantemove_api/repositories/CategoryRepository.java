77f3ad4d5a56910bf96805793f0d2678f33a31fe
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-04 13:43:28 -0300

    feat: add repository anotation to all repositories

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/CategoryRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.bcarsoftware.avantemove_api.repositories;
 
 import com.bcarsoftware.avantemove_api.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     public abstract List<Category> findCategoryByUserId(Long userId);
 }
--- src/main/java/com/bcarsoftware/avantemove_api/repositories/UserRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.bcarsoftware.avantemove_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User as u where (u.email=:username or u.username=:username) and u.password=:password")
     public abstract User makeUserLogin(String username, String password);
