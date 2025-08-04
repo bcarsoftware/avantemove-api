481a0bca9f128a3144fcbbf98f9a26a3649ed252
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-04 12:51:27 -0300

    feat: belief repository file created

---
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/BeliefRepository.java
@@ -0,0 +1,7 @@
+package com.bcarsoftware.avantemove_api.repositories;
+
+import com.bcarsoftware.avantemove_api.models.Belief;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeliefRepository extends JpaRepository<Belief, Long> {
+}
