1768ef37bfd89cafa20c142a87ad5d510bb61e9a
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-04 13:29:22 -0300

    feat: seal exp repository created

---
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/SealExpRepository.java
@@ -0,0 +1,7 @@
+package com.bcarsoftware.avantemove_api.repositories;
+
+import com.bcarsoftware.avantemove_api.models.SealExp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SealExpRepository extends JpaRepository<SealExp, Long> {
+}
