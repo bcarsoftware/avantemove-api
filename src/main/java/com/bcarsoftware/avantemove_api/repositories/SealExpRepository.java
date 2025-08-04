2485e72c1acfbbc4fc30e11d7414bea084b25250
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-04 13:35:19 -0300

    feat: seal repository implemented

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/SealExpRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/SealExpRepository.java
@@ -2,6 +2,10 @@ package com.bcarsoftware.avantemove_api.repositories;
 
 import com.bcarsoftware.avantemove_api.models.SealExp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SealExpRepository extends JpaRepository<SealExp, Long> {
+    @Query("select exp from SealExp as exp where exp.startXp<=:experience and exp.finishXp>=:experience")
+    public abstract SealExp findSealExpByUserExperience(@Param("experience") int experience);
 }
