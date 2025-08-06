2fcf88c5f31bb7f098f952f2d3b330f96e58ddf8
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-06 11:17:36 -0300

    feat: goal controller linked to service

--- src/main/java/com/bcarsoftware/avantemove_api/controllers/GoalController.java
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/GoalController.java
@@ -1,22 +1,41 @@
 package com.bcarsoftware.avantemove_api.controllers;
 
+import com.bcarsoftware.avantemove_api.core.responses.SuccessResponse;
 import com.bcarsoftware.avantemove_api.dtos.GoalDTO;
+import com.bcarsoftware.avantemove_api.models.Goal;
+import com.bcarsoftware.avantemove_api.services.GoalService;
+import com.bcarsoftware.avantemove_api.services.IGoalService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/goal")
 public class GoalController implements IGoalController {
+    private final IGoalService goalService = new GoalService();
+
     @Override
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody GoalDTO goalDTO) {
-        return null;
+        SuccessResponse<Goal> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.goalService.save(goalDTO));
+        successResponse.setCode(201);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Override
     @GetMapping("/{userId}/user")
     public ResponseEntity<?> getByUserId(@PathVariable Long userId) {
-        return null;
+        SuccessResponse<List<Goal>> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.goalService.getByUserId(userId));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
@@ -25,12 +44,22 @@ public class GoalController implements IGoalController {
         @PathVariable Long id,
         @RequestBody GoalDTO goalDTO
     ) {
-        return null;
+        SuccessResponse<Goal> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.goalService.update(id, goalDTO));
+        successResponse.setCode(201);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Override
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        return null;
+        SuccessResponse<Goal> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.goalService.delete(id));
+        successResponse.setCode(201);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 }
