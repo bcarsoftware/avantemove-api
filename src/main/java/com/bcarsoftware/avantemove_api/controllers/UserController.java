d851a9976580a0fc23b8e5f625a68d081c163f5c
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-06 11:33:56 -0300

    fix: user controller linked to its service

--- src/main/java/com/bcarsoftware/avantemove_api/controllers/UserController.java
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/UserController.java
@@ -1,63 +1,105 @@
 package com.bcarsoftware.avantemove_api.controllers;
 
+import com.bcarsoftware.avantemove_api.core.responses.SuccessResponse;
 import com.bcarsoftware.avantemove_api.dtos.LoginDTO;
 import com.bcarsoftware.avantemove_api.dtos.RecoveryDTO;
 import com.bcarsoftware.avantemove_api.dtos.UserDTO;
+import com.bcarsoftware.avantemove_api.models.User;
+import com.bcarsoftware.avantemove_api.services.IUserService;
+import com.bcarsoftware.avantemove_api.services.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController implements IUserController{
+    private final IUserService userService = new UserService();
+
     @Override
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody UserDTO userDTO) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.save(userDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Override
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.login(loginDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @PostMapping("/logout")
-    public ResponseEntity<?> logout(@RequestParam String token) {
-        return null;
+    public ResponseEntity<?> logout() {
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.logout());
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(
-        @PathVariable Integer id,
+        @PathVariable Long id,
         @RequestParam String token
     ) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.getById(id));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @PatchMapping("/{id}/update")
     public ResponseEntity<?> update(
-        @PathVariable Integer id,
+        @PathVariable Long id,
         @RequestBody UserDTO userDTO,
         @RequestParam String token
     ) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.update(id, userDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @PatchMapping("/recovery")
     public ResponseEntity<?> updatePassword(RecoveryDTO recoveryDTO) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.updatePassword(recoveryDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<?> delete(
-        @PathVariable Integer id,
+        @PathVariable Long id,
         @RequestParam String token
     ) {
-        return null;
+        SuccessResponse<User> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.userService.delete(id));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 }
