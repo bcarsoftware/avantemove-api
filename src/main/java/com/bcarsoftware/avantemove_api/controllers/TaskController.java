0f89a0e768d6956b6ab041516fc721df2c4fedee
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-07 11:11:58 -0300

    feat: task controller linked to service

--- src/main/java/com/bcarsoftware/avantemove_api/controllers/TaskController.java
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/TaskController.java
@@ -1,22 +1,41 @@
 package com.bcarsoftware.avantemove_api.controllers;
 
+import com.bcarsoftware.avantemove_api.core.responses.SuccessResponse;
 import com.bcarsoftware.avantemove_api.dtos.TaskDTO;
+import com.bcarsoftware.avantemove_api.models.Task;
+import com.bcarsoftware.avantemove_api.services.ITaskService;
+import com.bcarsoftware.avantemove_api.services.TaskService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController implements ITaskController {
+    private final ITaskService taskService = new TaskService();
+
     @Override
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody TaskDTO taskDTO) {
-        return null;
+        SuccessResponse<Task> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.taskService.save(taskDTO));
+        successResponse.setCode(201);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Override
     @GetMapping("/{habitId}/habit")
     public ResponseEntity<?> getTaskByHabit(@PathVariable Long habitId) {
-        return null;
+        SuccessResponse<List<Task>> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.taskService.getTaskByHabit(habitId));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
@@ -25,18 +44,33 @@ public class TaskController implements ITaskController {
         @PathVariable Long id,
         @RequestBody TaskDTO taskDTO
     ) {
-        return null;
+        SuccessResponse<Task> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.taskService.update(id, taskDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @PatchMapping("/{id}/finish")
     public ResponseEntity<?> finish(@PathVariable Long id) {
-        return null;
+        SuccessResponse<Task> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.taskService.finish(id));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        return null;
+        SuccessResponse<Task> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.taskService.delete(id));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 }
