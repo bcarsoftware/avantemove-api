256468ec315408ca020aced5e3faa1cfd168e21b
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-07 16:39:18 -0300

    fix: select just active goals

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/GoalRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/GoalRepository.java
@@ -2,13 +2,16 @@ package com.bcarsoftware.avantemove_api.repositories;
 
 import com.bcarsoftware.avantemove_api.models.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
-    public abstract List<Goal> findGoalByUserId(Long userId);
+    @Query("select g from Goal as g where g.active=true and g.user.id=:userId")
+    public abstract List<Goal> findGoalByUserId(@Param("userId") Long userId);
 
     public abstract Goal findFirstById(Long id);
 }
