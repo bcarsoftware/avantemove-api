c378d47cfddad4051ed382d0bf8843997e9ca90b
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-07 10:40:07 -0300

    feat: belief controller linked to service

--- src/main/java/com/bcarsoftware/avantemove_api/controllers/BeliefController.java
+++ src/main/java/com/bcarsoftware/avantemove_api/controllers/BeliefController.java
@@ -1,22 +1,41 @@
 package com.bcarsoftware.avantemove_api.controllers;
 
+import com.bcarsoftware.avantemove_api.core.responses.SuccessResponse;
 import com.bcarsoftware.avantemove_api.dtos.BeliefDTO;
+import com.bcarsoftware.avantemove_api.models.Belief;
+import com.bcarsoftware.avantemove_api.services.BeliefService;
+import com.bcarsoftware.avantemove_api.services.IBeliefService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/belief")
 public class BeliefController implements IBeliefController {
+    private final IBeliefService beliefService = new BeliefService();
+
     @Override
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody BeliefDTO beliefDTO) {
-        return null;
+        SuccessResponse<Belief> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.beliefService.save(beliefDTO));
+        successResponse.setCode(201);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Override
     @GetMapping("/{userId}/user")
     public ResponseEntity<?> getBeliefByUserId(@PathVariable Long userId) {
-        return null;
+        SuccessResponse<List<Belief>> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.beliefService.getBeliefByUserId(userId));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
@@ -25,12 +44,22 @@ public class BeliefController implements IBeliefController {
         @PathVariable Long id,
         @RequestBody BeliefDTO beliefDTO
     ) {
-        return null;
+        SuccessResponse<Belief> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.beliefService.update(id, beliefDTO));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 
     @Override
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        return null;
+        SuccessResponse<Belief> successResponse = new SuccessResponse<>();
+
+        successResponse.setData(this.beliefService.delete(id));
+        successResponse.setCode(200);
+
+        return ResponseEntity.status(HttpStatus.OK).body(successResponse);
     }
 }
