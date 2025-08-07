b2b22a00d703c65ebcaf806c73371dd27e750006
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-07 12:40:42 -0300

    feat: seal exp service interface created

---
+++ src/main/java/com/bcarsoftware/avantemove_api/services/ISealExpService.java
@@ -0,0 +1,13 @@
+package com.bcarsoftware.avantemove_api.services;
+
+import com.bcarsoftware.avantemove_api.dtos.SealExpDTO;
+import com.bcarsoftware.avantemove_api.models.SealExp;
+
+import java.util.List;
+
+public interface ISealExpService {
+    public abstract SealExp save(SealExpDTO sealExpDTO);
+    public abstract List<SealExp> getAllSealExp();
+    public abstract SealExp update(Long id, SealExpDTO sealExpDTO);
+    public abstract SealExp getSealExpByUserExperience(int experience);
+}
