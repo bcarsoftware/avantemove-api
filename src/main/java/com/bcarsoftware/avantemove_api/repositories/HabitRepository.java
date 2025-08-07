660748592495d1883d32e0d0bebb80bdf6a9c014
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-07 16:43:18 -0300

    fix: select just active habits

--- src/main/java/com/bcarsoftware/avantemove_api/repositories/HabitRepository.java
+++ src/main/java/com/bcarsoftware/avantemove_api/repositories/HabitRepository.java
@@ -2,14 +2,20 @@ package com.bcarsoftware.avantemove_api.repositories;
 
 import com.bcarsoftware.avantemove_api.models.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface HabitRepository extends JpaRepository<Habit, Long> {
-    public abstract List<Habit> findHabitByUserId(Long id);
-    public abstract List<Habit> findHabitByGoalId(Long id);
+    @Query("select h from Habit as h where h.active=true and h.user.id=:id")
+    public abstract List<Habit> findHabitByUserId(@Param("id") Long id);
 
-    public abstract Habit findFirstById(Long id);
+    @Query("select h from Habit as h where h.active=true and h.goal.id=:id")
+    public abstract List<Habit> findHabitByGoalId(@Param("id") Long id);
+
+    @Query("select h from Habit as h where h.active=true and h.id=:id")
+    public abstract Habit findFirstById(@Param("id") Long id);
 }
