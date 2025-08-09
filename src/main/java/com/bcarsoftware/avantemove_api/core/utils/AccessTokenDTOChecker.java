822b11f5a4dd0f79012cad088197155b446a0979
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-09 15:58:29 -0300

    feat: access token service and interface

--- src/main/java/com/bcarsoftware/avantemove_api/core/utils/AccessTokenDTOChecker.java
+++ src/main/java/com/bcarsoftware/avantemove_api/core/utils/AccessTokenDTOChecker.java
@@ -3,24 +3,28 @@ package com.bcarsoftware.avantemove_api.core.utils;
 import com.bcarsoftware.avantemove_api.dtos.AccessTokenDTO;
 import com.bcarsoftware.avantemove_api.exceptions.BodyException;
 
-import java.util.List;
-
 public class AccessTokenDTOChecker {
+    private static final String EMAIL_REGEX = "^[a-z][a-z0-9._-]{1,254}[a-z0-9]@[a-z0-9]{1,16}(.[a-z]{2,3}){1,2}$";
+    private static final String USERNAME_REGEX = "^[a-z][a-z-_0-9]{1,254}$";
+
+    public static void checkEmailAccessToken(AccessTokenDTO accessTokenDTO) {
+        if (!accessTokenDTO.username().matches(EMAIL_REGEX))
+            throw new BodyException("access token invalid email address");
+    }
+
+    public static void checkUsernameAccessToken(AccessTokenDTO accessTokenDTO) {
+        if (!accessTokenDTO.username().matches(USERNAME_REGEX))
+            throw new BodyException("access token invalid email address");
+    }
+
+    public static void checkTokenLength512(AccessTokenDTO accessTokenDTO) {
+        if (accessTokenDTO.token().length() > 512)
+            throw new BodyException("access token too long");
+    }
+
     public static void accessTokenDTOChecker(AccessTokenDTO accessTokenDTO) {
-        boolean tokenGreater512length = accessTokenDTO.token().length() > 512;
-
-        if (!tokenGreater512length)
-            throw new BodyException("token is too long");
-
-        String emailRegex = "^[a-z][a-z0-9._-]{1,254}[a-z0-9]@[a-z0-9]{1,16}(.[a-z]{2,3}){1,2}$";
-        String usernameRegex = "^[a-z][a-z-_0-9]{1,254}$";
-
-        List<Boolean> checkersUsername = List.of(
-            accessTokenDTO.username().matches(emailRegex),
-            accessTokenDTO.username().matches(usernameRegex)
-        );
-
-        if (!checkersUsername.contains(true))
-            throw new BodyException("username isn't a valid username or email");
+        checkTokenLength512(accessTokenDTO);
+        checkUsernameAccessToken(accessTokenDTO);
+        checkEmailAccessToken(accessTokenDTO);
     }
 }
---
+++ src/main/java/com/bcarsoftware/avantemove_api/services/AccessTokenService.java
@@ -0,0 +1,100 @@
+package com.bcarsoftware.avantemove_api.services;
+
+import com.bcarsoftware.avantemove_api.core.utils.AccessTokenDTOChecker;
+import com.bcarsoftware.avantemove_api.dtos.AccessTokenDTO;
+import com.bcarsoftware.avantemove_api.exceptions.AuthException;
+import com.bcarsoftware.avantemove_api.models.AccessToken;
+import com.bcarsoftware.avantemove_api.repositories.AccessTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccessTokenService implements IAccessTokenService {
+    @Autowired
+    private AccessTokenRepository accessTokenRepository;
+
+    @Override
+    public AccessToken save(AccessTokenDTO accessTokenDTO) {
+        AccessTokenDTOChecker.accessTokenDTOChecker(accessTokenDTO);
+
+        AccessToken accessToken = this.accessTokenRepository.findFirstByUsername(accessTokenDTO.username());
+
+        accessToken = accessToken == null ? new AccessToken() : accessToken;
+
+        accessToken.setUsername(accessTokenDTO.username());
+        accessToken.setToken(accessTokenDTO.token());
+
+        try {
+            return accessTokenRepository.save(accessToken);
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+
+            throw new AuthException("access token user not authorized");
+        }
+    }
+
+    @Override
+    public AccessToken getAccessTokenByUsername(AccessTokenDTO accessTokenDTO) {
+        AccessTokenDTOChecker.checkUsernameAccessToken(accessTokenDTO);
+
+        AccessToken accessToken = this.accessTokenRepository.findFirstByUsername(accessTokenDTO.username());
+
+        if (accessToken == null)
+            throw new AuthException("access token user not authorized");
+
+        return accessToken;
+    }
+
+    @Override
+    public AccessToken getAccessTokenByEmail(AccessTokenDTO accessTokenDTO) {
+        AccessTokenDTOChecker.checkEmailAccessToken(accessTokenDTO);
+
+        AccessToken accessToken = this.accessTokenRepository.findFirstByUsername(accessTokenDTO.username());
+
+        if (accessToken == null)
+            throw new AuthException("access token user not authorized");
+
+        return accessToken;
+    }
+
+    @Override
+    public AccessToken getAccessTokenByToken(AccessTokenDTO accessTokenDTO) {
+        AccessTokenDTOChecker.checkTokenLength512(accessTokenDTO);
+
+        AccessToken accessToken = this.accessTokenRepository.findFirstByToken(accessTokenDTO.token());
+
+        if (accessToken == null)
+            throw new AuthException("access token user not authorized");
+
+        return accessToken;
+    }
+
+    @Override
+    public String deleteAccessTokenByToken(AccessTokenDTO accessTokenDTO) {
+        AccessTokenDTOChecker.checkTokenLength512(accessTokenDTO);
+
+        int deleted = this.accessTokenRepository.deleteAccessTokenByToken(accessTokenDTO.token());
+
+        if (deleted == 0)
+            throw new AuthException("access token user not authorized");
+
+        return "access token deleted successfully";
+    }
+
+    @Override
+    public String deleteAccessTokenByUsernameOrEmail(String username, String email) {
+        AccessTokenDTO accessUsername = new AccessTokenDTO(username, null);
+        AccessTokenDTO accessEmail = new AccessTokenDTO(email, null);
+
+        AccessTokenDTOChecker.checkUsernameAccessToken(accessUsername);
+        AccessTokenDTOChecker.checkEmailAccessToken(accessEmail);
+
+        int deleted = this.accessTokenRepository.deleteAccessTokenByUsernameOrEmail(username, email);
+
+        if (deleted == 0)
+            throw new AuthException("access token user not authorized");
+
+        return "access tokens deleted successfully";
+    }
+}
