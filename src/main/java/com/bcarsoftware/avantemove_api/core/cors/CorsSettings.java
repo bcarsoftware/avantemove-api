17b7315d4389c05ddc8aa3c4da4d262b5b6d3f40
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-08-29 12:30:09 -0300

    infra: cors configuration

---
+++ src/main/java/com/bcarsoftware/avantemove_api/core/cors/CorsSettings.java
@@ -0,0 +1,29 @@
+package com.bcarsoftware.avantemove_api.core.cors;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsSettings {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost",
+                                "capacitor://localhost",
+                                "ionic://localhost",
+                                "http://localhost:8080",
+                                "http://localhost:8100"
+                        ).allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
